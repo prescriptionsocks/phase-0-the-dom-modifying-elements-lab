0995aae086dddfc876af47fbe16b82353a977791
Sam Baraka <barakaarunga@gmail.com>
2025-09-07 23:39:12 +0300

    complte lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+const main = document.querySelector('main#main')
+main.remove()
+
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+
+newHeader.textContent = 'Sam Baraka is the champion';
+
+
+
